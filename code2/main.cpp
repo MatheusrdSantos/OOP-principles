82d4038533d361dc11c8eb55aa3b4262dac4b6a2
MatheusrdSantos <matheus.r.d.s10@hotmail.com>
2019-03-21 21:32:22 -0300

    main is ready to test a game

--- code2/main.cpp
+++ code2/main.cpp
@@ -6,7 +6,14 @@
 
 int main(int argc, char const *argv[]){
 
-    Player p("Matheus");
-    cout<<p.throwDices()<<endl;
+    Player p1("Matheus");
+    Player p2("Joao");
+    Player p3("Ricardo");
+    GameController game(50);
+    game.addPlayer(&p1);
+    game.addPlayer(&p2);
+    game.addPlayer(&p3);
+    game.start();
+    //cout<<p.throwDices()<<endl;
     return 0;
 }
